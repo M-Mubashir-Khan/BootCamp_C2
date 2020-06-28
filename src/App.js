a540c842bb837bf613a0f73540e5414266cb2d5a
M-Mubashir-Khan <64747257+M-Mubashir-Khan@users.noreply.github.com>
2020-06-28 23:36:44 +0500

    Removed Label

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
 import Grid from '@material-ui/core/Grid';
-//import MainSection from './Components/MainSection'
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
